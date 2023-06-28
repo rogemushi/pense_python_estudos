c6d91414a4717adc9fa694befe44710054b352df
rogemushi <rogervianagoncalves.alencar@gmail.com>
2023-06-27 21:35:05 -0300

    adding chap 1

---
+++ cap_01/resumo.py
@@ -0,0 +1,3 @@
+# Capítulo mais destinado à principais fundamentos, como definição de programa, execução do python, este programa abaixo que é o nosso "primeiro", valores e tipos, demonstração sobre linguagens formais e naturais, e comentários sobre depuração.
+
+print("hello world")
